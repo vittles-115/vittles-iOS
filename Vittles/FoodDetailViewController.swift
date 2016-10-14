fc2c9cbd940908049b0acd3fb8768d93f25480d5
jennykwok <jennykwok95@yahoo.com>
2016-10-14 12:41:46 -0700

    food detail UI

---
+++ Vittles/FoodDetailViewController.swift
@@ -0,0 +1,76 @@
+//
+//  FoodDetailViewController.swift
+//  MenuApp
+//
+//  Created by Jenny Kwok on 10/13/16.
+//  Copyright © 2016 Jenny. All rights reserved.
+//
+
+import UIKit
+
+class FoodDetailViewController: UIViewController, UIScrollViewDelegate{
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var foodDescriptionLabel: UILabel!
+    @IBOutlet weak var showMoreButton: UIButton!
+    
+  
+    var selectedImageURL:String = ""
+    var selectedImage:UIImage?
+  
+    
+    //var loadedObjects:[MAReview] = [MAReview]()
+    
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Initialize Data Managers
+        
+        
+        addTopDivider(self.showMoreButton, thickness: 1, color: MA_BGGray)
+        
+        //Setup delegates
+        self.scrollView.delegate = self
+        //self.scrollView.contentSize.height = 2000
+        
+      
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y > 200{
+            //scrollView.bounces = false
+            //childTableVC?.tableView.isScrollEnabled = true
+        }else{
+            //scrollView.bounces = true
+            //childTableVC?.tableView.isScrollEnabled = false
+        }
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
