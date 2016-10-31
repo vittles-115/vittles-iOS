8d6e9c42cf07c9fe70f739d4961541da703ff3cb
jennykwok <jennykwok95@yahoo.com>
2016-10-30 21:20:45 -0700

    updated file tracking

---
+++ Vittles/MAProfileOptionTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MAProfileOptionTableViewCell.swift
+//  MenuApp
+//
+//  Created by Jenny Kwok on 2/26/16.
+//  Copyright © 2016 Jenny. All rights reserved.
+//
+
+import UIKit
+
+class MAProfileOptionTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var optionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
